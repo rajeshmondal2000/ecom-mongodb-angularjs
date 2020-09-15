a979854d2c4b97ba9168c8cf75d3c8fc26bfd49a
rajeshmondal2000 <rajeshmondal9007@gmail.com>
2020-09-15 19:26:33 +0530

    Cf

--- api/get-category.js
+++ api/get-category.js
@@ -2,9 +2,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://rajeshmondal9007@gmail.com:Rajesh@2000@cluster0.gaifp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
+var data = client.connect(err => {
+  const collection = client.db("test").collection("category");
   // perform actions on the collection object
+  return collection
   client.close();
 });
 
@@ -12,6 +13,6 @@ client.connect(err => {
 module.exports = (req, res)=>{
   res.send({
     status: 200,
-    data: "Rajesh Rajesh Mondal"
+    data: data
   })
 }
