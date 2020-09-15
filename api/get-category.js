747333d79b7624e9c395260bf0c283808bcc0b1c
rajeshmondal2000 <rajeshmondal9007@gmail.com>
2020-09-15 19:36:11 +0530

    Bsbh

--- api/get-category.js
+++ api/get-category.js
@@ -1,7 +1,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://rajeshmondal9007@gmail.com:Rajesh@2000@cluster0.gaifp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 var data = client.connect(err => {
   const collection = client.db("test").collection("category");
   // perform actions on the collection object
