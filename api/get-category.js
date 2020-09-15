23af3314e679d5b54be747c79bbb09d15d073860
rajeshmondal2000 <rajeshmondal9007@gmail.com>
2020-09-15 20:53:14 +0530

    Hgg

--- api/get-category.js
+++ api/get-category.js
@@ -1,18 +1,47 @@
 
-const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://rajeshmondal9007@gmail.com:Rajesh@2000@cluster0.gaifp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-var data = client.connect(err => {
-  const collection = client.db("test").collection("category");
-  // perform actions on the collection object
-  return collection
-  client.close();
-});
 
+// Import Dependencies
+const url = require('url')
+const MongoClient = require('mongodb').MongoClient
 
-module.exports = (req, res)=>{
-  res.send({
-    status: 200,
-    data: data
-  })
+// Create cached connection variable
+let cachedDb = null
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+async function connectToDatabase(uri) {
+  // If the database connection is cached,
+  // use it instead of creating a new connection
+  if (cachedDb) {
+    return cachedDb
+  }
+
+  // If no connection is cached, create a new one
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+
+  // Select the database through the connection,
+  // using the database path of the connection string
+  const db = await client.db(url.parse(uri).pathname.substr(1))
+
+  // Cache the database connection and return the connection
+  cachedDb = db
+  return db
+}
+
+// The main, exported, function of the endpoint,
+// dealing with the request and subsequent response
+module.exports = async (req, res) => {
+  // Get a database connection, cached or otherwise,
+  // using the connection string environment variable as the argument
+  const db = await connectToDatabase(uri)
+
+  // Select the "users" collection from the database
+  const collection = await db.collection('category')
+
+  // Select the users collection from the database
+  const users = await collection.find({}).toArray()
+
+  // Respond with a JSON string of all users in the collection
+  res.status(200).json({ users })
 }
